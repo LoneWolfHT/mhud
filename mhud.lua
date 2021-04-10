e9acab634370de9fc48c5b2c999f5cc8fafd7ff7
LoneWolfHT <lonewolf04361@gmail.com>
2021-04-09 18:07:52 -0700

    Allow more sensible huddef key names

--- mhud.lua
+++ mhud.lua
@@ -10,6 +10,45 @@ local function Obj(player)
 	end
 end
 
+local function convert_def(def, type)
+	if type == "text" then
+		def.number = def.number or      def.color
+		def.size   = def.size   or {x = def.text_scale}
+	elseif type == "image" then
+		def.text  = def.text  or      def.texture
+		def.scale = def.scale or {x = def.image_scale}
+	elseif type == "statbar" then
+		if def.textures then
+			def.text  = def.textures[1]
+			def.text2 = def.textures[2]
+		else
+			def.text = def.text or def.texture
+		end
+
+		if def.lengths then
+			def.number = def.lengths[1]
+			def.item   = def.lengths[2]
+		else
+			def.number = def.number or def.length
+		end
+
+		def.size = def.size or def.force_image_size
+	elseif type == "inventory" then
+		def.text   = def.text   or def.listname
+		def.number = def.number or def.size
+		def.item   = def.item   or def.selected
+	elseif type == "waypoint" then
+		def.name   = def.name   or def.waypoint_text
+		def.text   = def.text   or def.suffix
+		def.number = def.number or def.color
+	elseif type == "image_waypoint" then
+		def.text  = def.text  or      def.texture
+		def.scale = def.scale or {x = def.image_scale}
+	end
+
+	return def
+end
+
 function hud.add(self, player, name, def)
 	player = Obj(player)
 	local pname = player:get_player_name()
@@ -22,6 +61,8 @@ function hud.add(self, player, name, def)
 	if not self.huds[pname] then
 		self.huds[pname] = {}
 	end
+
+	def = convert_def(def, def.hud_elem_type)
 
 	local id = player:hud_add(def)
 
@@ -47,8 +88,11 @@ function hud.change(self, player, name, def)
 
 	assert(self.huds[pname][name], "Attempt to change hud that doesn't exist!")
 
+	def = convert_def(def, def.hud_elem_type or self.huds[pname][name].def.hud_elem_type)
+
 	for stat, val in pairs(def) do
 		player:hud_change(self.huds[pname][name].id, stat, val)
+		self.huds[pname][name].def[stat] = val
 	end
 end
 
